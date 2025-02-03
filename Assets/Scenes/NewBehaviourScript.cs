104902b312cd664e142a3bdcff9ca8a300077d2d
Melling <bluebell1183@gmail.com>
2025-02-04 00:38:27 +0900

    test: 한글 주석 상태 확인

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 나는 한글이에용
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
